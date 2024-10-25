934b61e7b4543a368f367044bd33d4a8d2c2687e
Warren Christian <wchristian2000@gmail.com>
2024-10-25 11:14:47 -0700

    Initial Commit

---
+++ NIO-Pipeline-FTP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NIO-Pipeline-FTP
+//
+//  Created by Warren Christian on 10/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NIO-Pipeline-FTP/NIO_Pipeline_FTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  NIO_Pipeline_FTPApp.swift
+//  NIO-Pipeline-FTP
+//
+//  Created by Warren Christian on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct NIO_Pipeline_FTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
